e5ba4f6b8d60106274bd5838413800aff73262c7
Yeslem Kh <23032@supnum.mr>
2025-12-14 17:33:39 +0000

    Automatic backpropagation has been implemented successfully via backward func

--- nn.py
+++ nn.py
@@ -58,21 +58,51 @@ class Value:
         self.grad = 0.0
         self._prev = set(_children)
         self._op = _op
+        self._backward = lambda: None
         self.label = label
 
     def __repr__(self):
         return f"Value(data {self.data})"
     def __add__(self, other):
         out = Value(self.data + other.data ,  (self, other), _op='+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
         return out
+    
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), _op='*')
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
         return out
+    
     def tanh(self):
         x = self.data
         t = ((math.exp(2*x) - 1) / (math.exp(2*x) + 1)) 
         out = Value(t, (self,) , 'tanh')
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
         return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
+
        
 # h=0.0001
 # a = Value(2.0, label='a')
@@ -176,6 +206,7 @@ def lol():
 
 
 
+
 #inputs 
 x1 = Value(2.0, label='x1')
 x2 = Value(0.0, label='x2')
@@ -189,24 +220,49 @@ x1w1 = x1*w1
 x1w1.label = 'x1w1'
 x2w2 = x2*w2
 x2w2.label = 'x2w2'
-x1w1x2w2 = x1*w1 + x2*w2
+x1w1x2w2 = x1w1 + x2w2
 x1w1x2w2.label = 'x1*w1 + x2*w2'
 n = x1w1x2w2 + b
 n.label= 'n'
+
 o = n.tanh()
 o.label='o'
 
-o.grad = 1.0
-n.grad = o.data ** 2
-x1w1x2w2.grad = n.grad * 1
-b.grad = n.grad * 1
-x1w1.grad = b.grad * 1
-x2w2.grad = b.grad * 1
-x1.grad = x1w1.grad * w1.data
-w1.grad = x1w1.grad * x1.data
-x2.grad = x2w2.grad * w2.data
-w2.grad = x2w2.grad * x2.data
+# o.grad = 1.0
+## backward manually
+# o._backward()
+# n._backward()
+# b._backward()
+# x1w1x2w2._backward()
+# x1w1._backward()
+# x2w2._backward()
 
 
+# n.grad = o.data ** 2
+# x1w1x2w2.grad = n.grad * 1
+# b.grad = n.grad * 1
+# x1w1.grad = b.grad * 1
+# x2w2.grad = b.grad * 1
+# x1.grad = x1w1.grad * w1.data
+# w1.grad = x1w1.grad * x1.data
+# x2.grad = x2w2.grad * w2.data
+# w2.grad = x2w2.grad * x2.data
+
+
+#topological & automatic backward
+# topo = []
+# visited = set()
+# def build_topo(v):
+#     if v not in visited:
+#         visited.add(v)
+#         for child in v._prev:
+#             build_topo(child)
+#         topo.append(v)
+# build_topo(o)
+# print(topo)
+# for i in reversed(topo):
+#     i._backward()
+#     # print(i)
+o.backward()
 dot = draw_dot(o)
 dot.render('graph', view=True)  
